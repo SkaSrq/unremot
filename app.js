ae9e50ab9f40203311d5524b51ed2a1acba9cc80
Sharique Celebzo <celebzo.com@gmail.com>
2023-05-12 23:10:15 +0530

    basic structure

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dotenv = require('dotenv');
+dotenv.config();
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.json());
+
+app.use(
+    cors({
+      origin: process.env.CLIENT_URL,
+      methods: "GET,POST,PUT,DELETE",
+      credentials: true
+    })
+  );
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
